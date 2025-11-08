c3b8160f324c1b1850fa528d2dcc8bfdef170644
Gabriel Hipólito <hipolitodeveloper@gmail.com>
2025-11-08 01:45:43 -0300

    vercel config

---
+++ api/app.py
@@ -0,0 +1,10 @@
+# api/app.py
+from fastapi import FastAPI
+from src.api import routes
+
+app = FastAPI(title="Valter - Expiry/Out-of-Stock")
+app.include_router(routes.router)
+
+@app.get("/ping")
+def ping():
+    return {"ok": True}
